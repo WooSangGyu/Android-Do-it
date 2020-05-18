47bab5f7bc91dc06d75993ad6dfd07ae306c1700
WooSangGyu <wsg0701@naver.com>
2020-05-18 17:06:16 +0900

    ScrollView change Image

--- Android do it 4/app/src/main/java/com/example/androiddoit4/MainActivity.java
+++ Android do it 4/app/src/main/java/com/example/androiddoit4/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.androiddoit4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ScrollView scrollView;
+    ImageView imageView;
+    BitmapDrawable bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scrollView = findViewById(R.id.Scroll);
+        imageView = findViewById(R.id.picture);
+        scrollView.setHorizontalScrollBarEnabled(true);
+
+        Resources res = getResources();
+        bitmap =(BitmapDrawable) res.getDrawable(R.drawable.p6);
+        int bitmapWidth = bitmap.getIntrinsicWidth();
+        int bitmapHeight = bitmap.getIntrinsicHeight();
+
+        imageView.setImageDrawable(bitmap);
+        imageView.getLayoutParams().width = bitmapWidth;
+        imageView.getLayoutParams().height = bitmapHeight;
+    }
+
+    public void ch(View v){
+        changeImage();
+    }
+
+    private void changeImage(){
+        Resources res = getResources();
+        bitmap =(BitmapDrawable) res.getDrawable(R.drawable.p8);
+        int bitmapWidth = bitmap.getIntrinsicWidth();
+        int bitmapHeight = bitmap.getIntrinsicHeight();
+
+        imageView.setImageDrawable(bitmap);
+        imageView.getLayoutParams().width = bitmapWidth;
+        imageView.getLayoutParams().height = bitmapHeight;
     }
 }
