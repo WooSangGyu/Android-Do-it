c249ce59c0c489e150ac454714c6c063c423a04e
WooSangGyu <wsg0701@naver.com>
2020-05-20 18:07:36 +0900

    make Activity orientation fix function

--- AndroidDoitPractice/app/src/main/java/com/example/androiddoitpractice/Orientation2.java
+++ AndroidDoitPractice/app/src/main/java/com/example/androiddoitpractice/Orientation2.java
@@ -4,47 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
-public class Orientation extends AppCompatActivity {
-
-    String name;
-
-    EditText editText;
-
+public class Orientation2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); //메모리 할당
-        setContentView(R.layout.activity_orientation);
-
-        showToast("onCreate 호출됨.");
-
-        editText = findViewById(R.id.editText);
-
-        Button button = findViewById(R.id.button3);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                name = editText.getText().toString();   // 버튼 클릭 시 name에 변수 값 할당
-                showToast("입력된 값을 변수에 저장했습니다 : " + name);
-            }
-        });
-
-        if (savedInstanceState != null){ //화면이 초기화 될 때 name 변수의 값을 복원
-            name = savedInstanceState.getString("name");
-            showToast("값을 복원했습니다 : " + name);
-        }
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        outState.putString("name", name); //변수의 값 저장
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orientation2);
     }
 
     @Override
